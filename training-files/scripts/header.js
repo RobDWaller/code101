b09d048f02f45d42e7186eae8aba864c2b91bee8
Rob Waller <rob.waller@breakthroughmedia.org>
2018-05-22 11:36:08 +0100

    Made improvements to documentation and file structure defined by si dawson

---
+++ training-files/scripts/header.js
@@ -0,0 +1,17 @@
+/**
+ * Add header text to the HTML page, this just adds a line of text to the H1
+ * element to the HTML page
+ *
+ * @author Rob Waller <rdwaller1984@googlemail.com>
+ */
+var header = function () {
+
+    // Create a header variable and assign it the value you wish.
+    var header = '';
+
+    // Get the H1 header element from the HTML page
+    //var headerElement = document.getElementById();
+
+    // Set the header element's value to match the header variable
+    //headerElement.innerHTML = ;
+};
